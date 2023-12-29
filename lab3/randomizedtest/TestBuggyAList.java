1ef2ad2496fb7de9a7df6754d99364134da26312
loc-codes <lachie.s.young@gmail.com>
2023-12-29 06:48:13 +0000

    Sync Lab and Project Updates

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -24,10 +24,45 @@ public class TestBuggyAList {
         truth.addLast(4);
         truth.addLast(5);
         truth.addLast(6);
-        assertEquals(buggy, truth);
+        assertEquals(buggy.size(), truth.size());
 
-        buggy.removeLast();
-        truth.removeLast();
-        assertEquals(buggy, truth);
+        assertEquals(truth.removeLast(), buggy.removeLast());
+        assertEquals(truth.removeLast(), buggy.removeLast());
+        assertEquals(truth.removeLast(), buggy.removeLast());
+    }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> noResize = new AListNoResizing<>();
+        BuggyAList<Integer> buggy = new BuggyAList<>();
+
+        int N = 50000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                noResize.addLast(randVal);
+                buggy.addLast(randVal);
+                assertEquals(noResize.size(), buggy.size());
+                //System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                int size = noResize.size();
+                //System.out.println("size: " + size);
+            } else if (operationNumber == 2 && noResize.size() > 0) {
+                int last1 = noResize.getLast();
+                int last2 = buggy.getLast();
+                assertEquals(last1, last2);
+                assertEquals(noResize.size(), buggy.size());
+                //System.out.println("getLast() returned " + last1);
+            } else if (operationNumber == 3 && noResize.size() > 0) {
+                int last1 = noResize.removeLast();
+                int last2 = buggy.removeLast();
+                assertEquals(last1, last2);
+                assertEquals(noResize.size(), buggy.size());
+                //System.out.println("removeLast() removed " + last1);
+            }
+        }
     }
 }
